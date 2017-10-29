5bedf22717cde003d8db92c6c8fc5be790605546
Abby Krishnan <abbykrishnan@Abby-Ks-MacBook-Air.local>
2017-10-29 02:11:06 -0500

    sample with form page

--- app/routes.py
+++ app/routes.py
@@ -35,14 +35,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('form.html')
 
 @app.route('/', methods = ['POST'])
 def home2():
-    name = request.form['name']
-
-
-
+    print("hey")
 
 if __name__ == '__main__':
   app.run(debug=True)
