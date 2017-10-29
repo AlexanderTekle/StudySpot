1086cd3067038c8b0871e21742877919803ed184
Alexander Tekle <tekle.alexander@gmail.com>
2017-10-29 05:43:37 -0500

    some tostring stuff, needs work on radio buttons parsing

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,7 @@ class StudySpace:
     groupConducive = False
     campus = ""
     distance = 0;
+    count = 0;
 
     def __init__(self, input):
         list = input.split("*")
@@ -23,6 +24,7 @@ class StudySpace:
         self.groupConducive = list[5]
         self.campus = list[6]
         self.distance = int(list[7])
+
 
 file = open("studySpaces.txt", "r")
 spaces = []
@@ -41,8 +43,24 @@ def home2():
     time = request.form['time']
     distance = request.form['distance']
     volume = request.form['volume']
-    print (time + distance + volume)
+    food = request.form['radio0']
+    onCampus = request.form['radio1']
+    group = request.form['radio2']
+    #my_array = [time, distance, volume]
+    print(contents())
+    print (time + " " + distance + " " + volume + " " + food + " " + onCampus + " " + group)
+   # attributes()
+
+
     return render_template('form.html')
+
+def contents():
+    spotLine = ""
+    x = 0
+    for space in spaces:
+        spotLine += ""+  str(x) + space.name + space.volume + str(space.groupConducive) + str(space.campus) + str(space.distance)
+        x + 1
+    return spotLine
 
 
 if __name__ == '__main__':
